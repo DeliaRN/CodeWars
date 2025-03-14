f6c8a101ddff3db8213bf90fe03309362be78c4d
delia_recio <delia_recio@epam.com>
2025-03-14 15:10:19 +0100

    Initial commit: First kata

---
+++ kata1/smallestInteger_test.go
@@ -0,0 +1,21 @@
+package kata1
+
+import "testing"
+
+func TestSmallestIntegerFinder(t *testing.T) {
+
+	tests := []struct {
+		numbers  []int
+		expected int
+	}{
+		{[]int{55, 96, 66, 1}, 1},
+		{[]int{34, -345, -1, -100}, -345},
+	}
+
+	for _, test := range tests {
+		result := SmallestIntegerFinder(test.numbers)
+		if result != test.expected {
+			t.Errorf("got %d want %d", result, test.expected)
+		}
+	}
+}
